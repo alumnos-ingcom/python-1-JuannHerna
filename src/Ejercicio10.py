0b813061dcbb28790478c3a194479af21447c423
Juan Hernandez <jh85733@gmail.com>
2022-05-25 20:09:19 -0300

    Ejercicio terminado

--- src/Ejercicio10.py
+++ src/Ejercicio10.py
@@ -8,3 +8,27 @@ Escribir una función que indique con True si una palabra o frase ingresada se t
 Palíndromo,es si se lee igual de derecha a izquierda que de izquierda a derecha.
 """
 def es_palindromo(texto):
+    text1=texto
+    maxx=len(text1)
+    tex_reves=''
+    cont=0
+    result= False
+    for i in range(maxx):
+        tex_reves+= text1[-i-1]    
+    
+    for j in range (maxx):
+        if tex_reves[j]== text1[j]:
+            cont+=1
+    if cont==maxx:
+        result= True
+    return result
+
+def principal():
+    print("Este programa determina si la palabra o frase es palindromo \nPalindromo es si se lee igual de derecha a izquierda que de izquierda a derecha")
+    print("Ingrese palabra a determinar: ")
+    palabra= input("")
+    palabra =palabra.lower()
+    res_palabra=es_palindromo(palabra)
+    print(f"Es palindroma: {res_palabra}")
+if __name__ == '__main__':
+    principal()
