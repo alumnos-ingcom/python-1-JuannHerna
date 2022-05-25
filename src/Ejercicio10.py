0cc626d6fb00591b68d9646c9222808a80c5b335
Juan <jh85733@gmail.com>
2022-05-25 18:41:35 -0300

    Subiendo archivos para modificar

---
+++ src/Ejercicio10.py
@@ -0,0 +1,10 @@
+###############
+# Juan cruz Hernandez- JuannHerna
+# UNRN Andina - Introducción a la Ingenieria en Computación
+###############
+
+"""
+Escribir una función que indique con True si una palabra o frase ingresada se trata de un palindromo.
+Palíndromo,es si se lee igual de derecha a izquierda que de izquierda a derecha.
+"""
+def es_palindromo(texto):
---
+++ src/Ejercicio11.py
@@ -0,0 +1,8 @@
+###############
+# Juan cruz Hernandez- JuannHerna
+# UNRN Andina - Introducción a la Ingenieria en Computación
+###############
+"""
+Escribir una función que indique con True si un número entero es multiplo de otro, utilizando sumas y restas.
+"""
+def es_multiplo(numero, multiplo):
