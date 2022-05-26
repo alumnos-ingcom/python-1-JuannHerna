9a7214ddb813d5f9a2b923f37facb476dd30f40e
Juan Hernandez <jh85733@gmail.com>
2022-05-25 21:43:00 -0300

    Subiendo ejercicio

--- src/Ejercicio11.py
+++ src/Ejercicio11.py
@@ -6,3 +6,25 @@
 Escribir una función que indique con True si un número entero es multiplo de otro, utilizando sumas y restas.
 """
 def es_multiplo(numero, multiplo):
+    aux=numero
+    cont=0
+    result= False
+    while aux>0:
+        result= aux-multiplo
+        aux= result
+    if result==0:
+        result= True
+        
+    return result
+    
+def principal():
+    print("Ingrese dos numero para determinar si es multiplo:")
+    num=int(input("Numero a averiguar: "))
+    multiplo=int(input("Multiplo: "))
+    result =es_multiplo(num,multiplo)
+    if result== True:
+        print(f"{num} es multiplo de {multiplo}")
+    else:
+        print(f"{num} no es multiplo de {multiplo}")
+if __name__ == '__main__':
+    principal()
