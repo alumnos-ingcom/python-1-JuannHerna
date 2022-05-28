6f5084b465be564f58707811fd9bf5bd834e1849
Juan <jh85733@gmail.com>
2022-05-28 15:09:42 -0300

    Subiendo test ejercicio 2

--- tests/test_ejercicio2_juannherna.py
+++ tests/test_ejercicio2_juannherna.py
@@ -7,7 +7,19 @@ from src.ejercicio2 import signo
 
 """ Test 1 """
 
-def test_busca_int():
-    numero= -23
-    funcion= signo(numero)
-    assert isinstans (funcion, 1), "Esto esta bien"
+def test_busca_positivo():
+    numero= 45
+    resulta= signo(numero)
+    assert isinstance(resulta, int), "el resultado debe ser un número entero"
+    assert resulta == 1, "El resultado esperado de un numero positivo"
+def test_busca_negativo():
+    numero= -45
+    result= signo(numero)
+    assert isinstance(result, int), "el resultado debe ser un número entero"
+    assert result == -1, "El resultado esperado de un numero negativo"
+    
+def test_busca_cero():
+    numero= 0
+    result= signo(numero)
+    assert isinstance(result, int), "el resultado debe ser un número entero"
+    assert result == 0, "Es el resultado esperado"
