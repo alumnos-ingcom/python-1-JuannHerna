2c512859699e841c6383bbb67e0aca1c65923a1d
Juan <jh85733@gmail.com>
2022-05-28 13:21:05 -0300

    Agregando ejercicio 7

---
+++ src/ejercicio7.py
@@ -0,0 +1,22 @@
+###############
+# Juan cruz Hernandez- JuannHerna
+# UNRN Andina - Introducción a la Ingenieria en Computación
+###############
+
+"""
+Escribir un programa que permita transformar un número solicitado expresado en grados, minutos y segundos a segundos a segundos.
+Y otra que haga el cambio en el sentido contrario, devolviendo una tuple.
+
+Recuerden que un grado son 60 minutos y un minuto son 60 segundos.
+"""
+def sexadecimal_a_decimal(horas, minutos, segundos):
+def decimal_a_sexadecimal(numero):
+
+def principal():
+    """
+    Descripción del ejercicio
+    """
+    pass
+
+if __name__ == "__main__":
+    principal()
