9b6a88225130c597c277ad98ad2051383051f9c2
Juan <jh85733@gmail.com>
2022-05-29 12:37:33 -0300

    Arregle comparar estaba mal escrito

--- tests/test_ejercicio3_juannherna.py
+++ tests/test_ejercicio3_juannherna.py
@@ -11,24 +11,24 @@ Retornar -1 si el primero es menor que el segundo
 Retornar 0 si son iguales
 Retornar 1 si el primero es mayor que el segundo
 """
-def primero_menor():
+def test_primero_menor():
     num1=4
     num2=6
-    resulta1 = comprar(num1,num2)
+    resulta1 = compara(num1,num2)
     assert isinstance(resulta1, int), "El resultado debe ser entero"
     assert resulta1 == -1, "El primer numero no es el menor"
 
-def busca_iguales():
+def test_busca_iguales():
     num1=4
     num2=4
-    resulta2 = comprar(num1,num2)
+    resulta2 = compara(num1,num2)
     assert isinstance(resulta2, int), "El resultado tiene que ser un entero"
-    assert resulta2==2, "Los numero no son iguales"
+    assert resulta2==0, "Los numero son iguales"
     
-def primero_mayor():
+def test_primero_mayor():
     num1=8
     num2=4
-    resulta3 = comprar(num1,num2)
+    resulta3 = compara(num1,num2)
     assert isinstance(resulta3, int), "El resultado tiene que ser entero"
     assert resulta3 == 1, "El primer numero no es el mayor"
     
