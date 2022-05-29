56c5c7e08f9e50f32833443a201a71b7779d7751
Juan <jh85733@gmail.com>
2022-05-29 12:43:28 -0300

    Subiendo test 4

---
+++ tests/test_ejercicio4_juannherna.py
@@ -0,0 +1,13 @@
+###############
+# Juan cruz Hernandez- JuannHerna
+# UNRN Andina - Introducción a la Ingenieria en Computación
+###############
+import pytest
+from src.ejercicio4 import suma_lenta
+
+def test_busca_int():
+    numero=56
+    num2= 4
+    result= suma_lenta(numero,num2)
+    assert isinstance(result,int), "El resultado debe ser positivo"
+    assert result== numero+num2, "El resultado debe ser igual a result"
