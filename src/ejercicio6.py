fb92d49cc003627df7ff3c58acf0d3cae84b93c7
Juan <jh85733@gmail.com>
2022-05-30 12:36:34 -0300

    sub archivo

--- src/ejercicio6.py
+++ src/ejercicio6.py
@@ -9,22 +9,25 @@ Y Viceversa
 """
 def ordenar_mayor_a_menor(uno, dos, tres):
     lista=[uno, dos, tres]
-    for j in range(1,len(lista)):
-        for i in range(len(lista)-1):
-            if lista[i]<= lista[i+1]:
-                aux= lista[i]
-                lista[i]=lista[i+1]
-                lista[i+1]=aux
+     for j in range(1,len(lista)):
+         for i in range(len(lista)-1):
+             if lista[i]<= lista[i+1]:
+                 aux= lista[i]
+                 lista[i]=lista[i+1]
+                 lista[i+1]=aux
     return lista
     
 def ordenar_menor_a_mayor(uno, dos, tres):
     lista=[uno, dos, tres]
+    cont=0
+    i=0
     for j in range(1,len(lista)):
         for i in range(len(lista)-1):
             if lista[i]>= lista[i+1]:
                 aux= lista[i]
                 lista[i]=lista[i+1]
                 lista[i+1]=aux
+
     return lista
     
 def principal():
@@ -34,10 +37,10 @@ def principal():
     num3=int(input("Numero 3: "))
     opcion= int(input("Ingrese como desea ordenar \n.1 Mayor a Menor \n.2 Menor a Mayor\n"))
     if opcion ==1:
-        array=ordenar_mayor_a_menor(num1,num2,num3)
+        lista=ordenar_mayor_a_menor(num1,num2,num3)
     elif opcion==2:
-        array=ordenar_menor_a_mayor(num1,num2,num3)
-    print(f"La lista ordenada queda de la siguiente forma: {array}")
+        lista=ordenar_menor_a_mayor(num1,num2,num3)
+    print(f"La lista ordenada queda de la siguiente forma: {lista}")
     
 if __name__ == "__main__":
     principal()
