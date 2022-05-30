1ff7f63e33979547b67dee287e4aba395ee40068
Juan <jh85733@gmail.com>
2022-05-30 11:29:54 -0300

    Puse valor negativo para ´sumar´

--- tests/test_ejercicio4_juannherna.py
+++ tests/test_ejercicio4_juannherna.py
@@ -7,7 +7,7 @@ from src.ejercicio4 import suma_lenta
 
 def test_busca_int():
     numero=56
-    num2= 4
+    num2= -4
     result= suma_lenta(numero,num2)
     assert isinstance(result,int), "El resultado debe ser positivo"
     assert result== numero+num2, "El resultado debe ser igual a result"
