710ee60b3ee8a389ef3896ba1b048681107ffc57
Juan <jh85733@gmail.com>
2022-05-30 13:06:04 -0300

    Cambiar a tupla

--- tests/test_ejercicio6_juannherna.py
+++ tests/test_ejercicio6_juannherna.py
@@ -6,13 +6,13 @@ import pytest
 from src.ejercicio6 import ordenar_menor_a_mayor, ordenar_mayor_a_menor
 
 def test_menor_a_mayor():
-    esperado=(1,2,3)
+    esperado=[1,2,3]
     uno, dos, tres= esperado
     result= ordenar_menor_a_mayor(uno,dos,tres)
     assert result == esperado , "El result no esta ordenado"
 
 def test_mayor_a_menor():
-    esperado=(5,4,3)
+    esperado=[5,4,3]
     uno,dos,tres=esperado
     result= ordenar_mayor_a_menor(uno,dos,tres)
     assert result == esperado , "El result no esta ordenado"
