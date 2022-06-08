34110cb0e04081e8ff7626f05d8f879f63d3890f
Juan <jh85733@gmail.com>
2022-06-08 20:31:19 -0300

    cambio a tuple

--- tests/test_ejercicio6_juannherna.py
+++ tests/test_ejercicio6_juannherna.py
@@ -9,10 +9,12 @@ def test_menor_a_mayor():
     esperado=(1,2,3)
     uno, dos, tres= esperado
     result= ordenar_menor_a_mayor(uno,dos,tres)
+    assert isinstance(result, tuple), "Se esperaba una tuple"
     assert result == esperado , "El result no esta ordenado"
 
 def test_mayor_a_menor():
     esperado=(5,4,3)
     uno,dos,tres=esperado
     result= ordenar_mayor_a_menor(uno,dos,tres)
+    assert isinstance(result, tuple), "Se esperaba una tuple"
     assert result == esperado , "El result no esta ordenado"
