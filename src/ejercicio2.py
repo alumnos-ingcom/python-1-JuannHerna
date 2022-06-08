e81cb9a035164785602303e0326dc0f34a82d824
Juan <jh85733@gmail.com>
2022-06-08 18:42:11 -0300

    Pre y post

--- src/ejercicio2.py
+++ src/ejercicio2.py
@@ -9,7 +9,9 @@ Escribir una función que reciba un número e indique si el mismo es positivo, n
 """
 def signo(numero):
     """
-    Determina el sigo de un numero retornando un valor numerico 1, -1, 0 
+    Determina el sigo de un numero retornando un valor numerico 1, -1, 0
+    Precodindicion: El numero deber ser entero
+    Postcondicion: Retorna numero dependiendo del signo
     """
     num= numero
     minimo=0
