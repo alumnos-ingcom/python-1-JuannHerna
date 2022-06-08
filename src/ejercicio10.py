13f6a5e3ca173ea52d24a84a42be2d309085990c
Juan <jh85733@gmail.com>
2022-06-08 19:59:13 -0300

    Asigno la comparacion directamente al retutn me ahorra el if

--- src/ejercicio10.py
+++ src/ejercicio10.py
@@ -10,21 +10,20 @@ Palíndromo,es si se lee igual de derecha a izquierda que de izquierda a derecha
 def es_palindromo(texto):
     """
     Esta funcion determina si un numero o una frase son palindromas
+    Precodindicion:
+    Postcondicion:
     """
     text1=texto
     maxx=len(text1)
     tex_reves=''
     cont=0
-    result= False
     for i in range(maxx):
         tex_reves+= text1[-i-1]    
     
     for j in range (maxx):
         if tex_reves[j]== text1[j]:
             cont+=1
-    if cont==maxx:
-        result= True
-    return result
+    return cont==maxx
 
 def principal():
     print("Este programa determina si la palabra o frase es palindromo \nPalindromo es si se lee igual de derecha a izquierda que de izquierda a derecha")
