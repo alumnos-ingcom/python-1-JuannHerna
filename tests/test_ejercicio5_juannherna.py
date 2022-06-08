8f93af4496c1b0cd888bbf08f1993c78b800eb75
Juan <jh85733@gmail.com>
2022-06-08 19:47:14 -0300

    Saque funcion de resto, y cambie el tipo de dato a tuple

--- tests/test_ejercicio5_juannherna.py
+++ tests/test_ejercicio5_juannherna.py
@@ -3,18 +3,11 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ###############
 import pytest
-from src.ejercicio5 import division_lenta, resto
+from src.ejercicio5 import division_lenta
 
 def test_division_cociente():
     num1= 17
     num2= 3
     result= division_lenta(num1,num2)
-    assert isinstance(result, int), "El numero deberia ser flotante"
-    assert result >= 1, "El cociente no puede ser menor a 1" 
-    
-def test_busca_resto():
-    num1= 17
-    num2= 3
-    result= resto(num1,num2)
-    assert isinstance(result, int), "El resultado tiene que ser un numero entero"
-    assert result>0, "El resultado debe ser mayor a 0"
+    assert isinstance(result, tuple), "El resultado esperado deberia ser una tupla"
+    assert result >= 1, "El cociente no puede ser menor a 1"
